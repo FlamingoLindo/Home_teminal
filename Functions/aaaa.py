83e7cbadbce646766479bef413c7a38807c71470
FlamingoLindo <101421364+FlamingoLindo@users.noreply.github.com>
2024-08-20 14:54:19 -0300

    magic?

--- Functions/aaaa.py
+++ Functions/aaaa.py
@@ -3,31 +3,16 @@ import json
 from PIL import Image
 from io import BytesIO
 
-# API endpoint
-api_url = 'https://api.waifu.im/search'
-params = {
-    'is_nsfw': False,
-}
-response = requests.get(api_url, params=params)
+# Get data from the APIhttps://scryfall.com/docs/api/lists
+req = requests.get("https://api.magicthegathering.io/v1/cards/:id")
+test = json.loads(req.text)
 
-if response.status_code == 200:
-    data = json.loads(response.text)
+# Print the raw data to inspect it
+#print(test)
 
-    # Extract the image URL from the JSON response
-    image_url = data['images'][0]['url']
-    
-    # Make a new request to the image URL to get the image data
-    image_response = requests.get(image_url)
-    
-    if image_response.status_code == 200:
-        img_data = BytesIO(image_response.content)
-        
-        # Open the image with PIL
-        img = Image.open(img_data)
-        
-        # Display the image
-        img.show()
-    else:
-        print('Failed to retrieve the image. Status code:', image_response.status_code)
-else:
-    print('Request failed with status code:', response.status_code)
+# Extract the image URL
+# Assuming the first card in the list is the one you're interested in
+image_url = test['cards'][0]['imageUrl']
+
+
+print(image_url)
